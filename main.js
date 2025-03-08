8b98bbf0016a361649ede44387de6cde1bf0ec52
Suxrobbek2012 <suxrobbek714@gmail.com>
2025-03-08 10:55:50 +0500

    mushuk

---
+++ main.js
@@ -0,0 +1,9 @@
+function black(){
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+}
+function white(){
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+
+}
